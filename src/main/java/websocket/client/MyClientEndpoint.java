745744fcf5dcf9a0ac75cba394c5b12a6aaeb09c
egyud_v <vivien.egyud@gmail.com>
2017-03-10 09:29:05 +0100

    Message model added

--- src/main/java/websocket/client/MyClientEndpoint.java
+++ src/main/java/websocket/client/MyClientEndpoint.java
@@ -3,29 +3,36 @@ package websocket.client;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.websocket.ClientEndpoint;
-import javax.websocket.OnError;
-import javax.websocket.OnMessage;
-import javax.websocket.OnOpen;
-import javax.websocket.Session;
+import javax.websocket.*;
 
-@ClientEndpoint
+@ClientEndpoint(
+        encoders = { MessageEncoder.class },
+        decoders = { MessageDecoder.class }
+)
 public class MyClientEndpoint {
+
     @OnOpen
     public void onOpen(Session session) {
         System.out.println("Connected to endpoint: " + session.getBasicRemote());
+
         try {
-            String message = "a message to the Server";
-            System.out.println("Sending message to endpoint: " + message);
-            session.getBasicRemote().sendText(message);
-        } catch (IOException ex) {
+            Message response = new Message();
+            response.setParam1("Very first message from client");
+            response.setParam2("Param2");
+            System.out.println("Message to server: " + response.getParam1());
+            session.getBasicRemote().sendObject(response);
+        } catch (IOException | EncodeException ex) {
             Logger.getLogger(MyClientEndpoint.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
     @OnMessage
-    public void processMessage(String message, Session session) throws IOException {
-        System.out.println("Received message in client: " + message);
+    public void message(Message message, Session session) throws IOException, EncodeException {
+        System.out.println(message.getParam1());
+        Message response = new Message();
+        response.setParam1("From client new message");
+        response.setParam2("Param2");
+        session.getBasicRemote().sendObject(response);
         Client.messageLatch.countDown();
     }
 
